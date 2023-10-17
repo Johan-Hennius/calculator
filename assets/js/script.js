7c59fe5a4529b41d818926465c10d4e66f84e379
Johan Hennius <johan.bh19@gmail.com>
2023-10-17 09:50:14 +0000

    finish html and css

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const display = document.getElementById("display");
+    const buttons = ["7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "C", "0", "=", "/"];
+    const calculatorButtons = document.getElementById("calculator-buttons");
+    
+
+});
